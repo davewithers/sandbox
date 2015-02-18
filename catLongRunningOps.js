930bf445b402d7970a56fe47165aad30fbbf5a9d
davewithers <dwithers@spireon.com>
2015-02-18 15:15:37 -0600

    Create catLongRunningOps.js

---
+++ catLongRunningOps.js
@@ -0,0 +1,14 @@
+--catalog SLOW mongoDB queries
+catLongRunningOps = function(maxSecsRunning) {
+    db.currentOp().inprog.forEach(
+        function(d) {
+            if (d.secs_running > maxSecsRunning && d.op == "query" && !d.ns.startsWith("local") && d.ns.match(/procon.*/)) {
+                print("---------------------------")   
+                print("secs_running:", d.secs_running)
+                print("opid:", d.opid)
+                print("host:", d.client)
+                print("collection:", d.ns)   
+                printjson(d.query);
+            }
+        })
+};
